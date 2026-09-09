b5e22404656cb3b795c7ac02ab404b5c77250630
InakoDev <inako.studio+gh@gmail.com>
2026-09-09 03:37:47 +0100

    feat: add working tab switching and better sidebar nav styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,35 @@ import "./styles/themes/dark.css";
 import "./styles/main.css";
 
 import { getCurrentWindow } from "@tauri-apps/api/window";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
+    const [activeTab, setActiveTab] = useState("lessons");
+
     const appWindow = getCurrentWindow();
+
+    function tabChange(tab) {
+        setActiveTab(tab);
+    }
+
+    function renderTab() {
+        if (activeTab === "lessons") {
+            // return <h1>Lessons</h1>;
+        }
+        if (activeTab === "chat") {
+            return <h1>Meow</h1>;
+        }
+        if (activeTab === "progress") {
+            // return <h1>Progress</h1>;
+        }
+        return null;
+    }
 
     return (
         <main className="container">
-            <Sidebar activeTab="lessons" />
-            {/*<h1>Welcome to Tauri + React</h1>*/}
+            <Sidebar activeTab={activeTab} onTabChange={tabChange} />
+            {renderTab()}
         </main>
     );
 }
